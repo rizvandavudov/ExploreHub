297a263a9c528fc26a5bb6db7a68c788f53cfda2
rizvandavudov <rizv>
2026-08-28 22:03:32 +0400

    Display home categories with centered images

--- app/src/main/java/com/rizvandavudov/explorehub/ui/home/components/CategoryCard.kt
+++ app/src/main/java/com/rizvandavudov/explorehub/ui/home/components/CategoryCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -42,7 +43,8 @@ fun CategoryCard(
         )
     ) {
         Box(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
             Image(
                 painter = painterResource(
